176123911e936c9a8aa5e3b0750aab027229248c
BeatrizPlacido <127264395+BeatrizPlacido@users.noreply.github.com>
2023-10-02 21:25:45 -0300

    2.02: Criação da classe TurmaDTO

---
+++ TGSync/src/main/java/Model/DTO/TurmaDTO.java
@@ -0,0 +1,34 @@
+package Model.DTO;
+
+public class TurmaDTO {
+	private int id;
+	private int ano;
+	private int semestre;
+	private int disciplina;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getAno() {
+		return ano;
+	}
+	public void setAno(int ano) {
+		this.ano = ano;
+	}
+	public int getSemestre() {
+		return semestre;
+	}
+	public void setSemestre(int semestre) {
+		this.semestre = semestre;
+	}
+	public int getDisciplina() {
+		return disciplina;
+	}
+	public void setDisciplina(int disciplina) {
+		this.disciplina = disciplina;
+	}
+}
