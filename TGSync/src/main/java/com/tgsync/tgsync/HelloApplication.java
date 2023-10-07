c45d4c1b2b05d90786877ce51ae25307b6811d16
Gilvane Amaro <gilvane.amaro.jr@gmail.com>
2023-10-07 12:29:12 -0300

    :feat: 2.08: adicionado JfileChooser

--- TGSync/src/main/java/com/tgsync/tgsync/HelloApplication.java
+++ TGSync/src/main/java/com/tgsync/tgsync/HelloApplication.java
@@ -3,20 +3,29 @@ package com.tgsync.tgsync;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+    private static Scene mainScene;
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1200, 800);
+
+        mainScene = new Scene(fxmlLoader.load(), 1200, 800);
+
         stage.setTitle("TGSync");
-        stage.setScene(scene);
+        stage.setScene(mainScene);
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("/assets/logo-tgsync.png")));
+
         stage.show();
     }
 
+    public static Scene getMainScene(){
+        return mainScene;
+    }
     public static void main(String[] args) {
         launch();
     }
--- TGSync/src/main/java/com/tgsync/tgsync/HelloController.java
+++ TGSync/src/main/java/com/tgsync/tgsync/HelloController.java
@@ -1,16 +1,53 @@
 package com.tgsync.tgsync;
 
 import Model.DAO.SistemaDAO;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.FileChooser;
+
+import java.io.File;
+
+//import static jdk.jfr.consumer.EventStream.openFile;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button onOpenCSVButton;
 
+    final FileChooser fileChooser = new FileChooser();
+    @FXML
+    protected void onOpenCSVButton() {
+        Scene mainScene = HelloApplication.getMainScene();
+        onOpenCSVButton.setOnAction(
+                new EventHandler<ActionEvent>() {
+                    @Override
+                    public void handle(final ActionEvent e) {
+                        configureFileChooser(fileChooser);
+                        File file = fileChooser.showOpenDialog(mainScene.getWindow());
+                        if (file != null) {
+                            openFile(file);
+                        }
+                    }
+                });
+
+    }
+    private void openFile(File file) {
+        System.out.println("Caminho: "+ file.getAbsolutePath());
+    }
+
+
+    private static void configureFileChooser(final FileChooser fileChooser) {
+
+        fileChooser.setTitle("Abrir CSV");
+        fileChooser.setInitialDirectory(new File(System.getProperty("user.home"))
+        );
+    }
+    @FXML
+    protected void onViewAllDataButton(){
+        System.out.println("Visualizar dados");
     }
 }
