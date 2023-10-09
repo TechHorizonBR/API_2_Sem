b96cf975a9a47c003f4e213ba2e204e9bf98e0e5
Raul Neto <rjbnetto@gmail.com>
2023-10-09 20:07:12 -0300

    :feat: 2.15. Criação do Visual TelaOrientador.

--- TGSync/src/main/java/com/tgsync/tgsync/TelaOrientadorController.java
+++ TGSync/src/main/java/com/tgsync/tgsync/TelaOrientadorController.java
@@ -1,15 +1,43 @@
 package com.tgsync.tgsync;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class TelaOrientadorController {
 
     // Buttons
     @FXML
     private Button ButtonAlunos;
+    @FXML
+    private void abrirTelaAlunos(ActionEvent event) {
+        try {
+            // Carregue o arquivo FXML da tela de alunos
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("hello-view.fxml"));
+            Parent root = loader.load();
+
+            // Crie uma nova janela (Stage)
+            Stage alunosStage = new Stage();
+            alunosStage.setTitle("Tela de Alunos");
+
+            // Defina a cena da nova janela com o conteúdo do arquivo FXML carregado
+            Scene scene = new Scene(root);
+            alunosStage.setScene(scene);
+
+            // Mostre a nova janela
+            alunosStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     @FXML
     private Button ButtonCadastro;
     @FXML
@@ -19,11 +47,11 @@ public class TelaOrientadorController {
     @FXML
     private Button ButtonCadastrar;
 
-    // LabelText
+    // TextField
     @FXML
-    private Label labelNomeOrientador;
+    private TextField textFieldNomeOrientador;
     @FXML
-    private Label labelEmail;
+    private TextField textFieldEmail;
 
     // ListView
     @FXML
