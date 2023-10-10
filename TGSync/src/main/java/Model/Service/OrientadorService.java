25211d87ce11ec6ae7de62215b851d5ec6c7c2ad
Gilvane Amaro <gilvane.amaro.jr@gmail.com>
2023-10-10 10:02:32 -0300

    :feat: 2.18.1: classe OrientadorService + Alerts

---
+++ TGSync/src/main/java/Model/Service/OrientadorService.java
@@ -0,0 +1,4 @@
+package Model.Service;
+
+public class OrientadorService {
+}
--- TGSync/src/main/java/com/tgsync/tgsync/HelloController.java
+++ TGSync/src/main/java/com/tgsync/tgsync/HelloController.java
@@ -2,15 +2,20 @@ package com.tgsync.tgsync;
 
 import Model.DAO.SistemaDAO;
 import Model.Service.TgService;
+import Model.util.Alerts;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.FileChooser;
 
 import java.io.File;
+import java.io.IOException;
 
 //import static jdk.jfr.consumer.EventStream.openFile;
 
@@ -52,5 +57,21 @@ public class HelloController {
     @FXML
     protected void onViewAllDataButton(){
         System.out.println("Visualizar dados");
+    }
+
+
+    private void loadView(String absoluteName) {
+
+
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+            AnchorPane newAnchorPane = loader.load();
+            Scene mainScene = HelloApplication.getMainScene();
+
+        }
+        catch (IOException e){
+            Alerts.showAlert("IO Exeception","Erro carregar página", e.getMessage(), Alert.AlertType.ERROR);
+
+        }
     }
 }
