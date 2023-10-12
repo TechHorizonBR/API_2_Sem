0b9a2fa4c82003566adc68ced79d5250072d9bfe
viniciusmangaba <viniciusmangaba.ti@gmail.com>
2023-10-12 10:01:43 -0300

    :feat: 2.14: Atualização logo e botões Tela Alunos

--- TGSync/src/main/java/Test/AlunoTest.java
+++ TGSync/src/main/java/Test/AlunoTest.java
@@ -10,18 +10,18 @@ import java.util.List;
 
 public class AlunoTest {
     public static void main(String[] args) {
-        AlunoDAO alunoDAO = new AlunoDAO();
+        //AlunoDAO alunoDAO = new AlunoDAO();
         //System.out.println(alunoDAO.getAllAlunos());
         //System.out.println(alunoDAO.getAlunoPorEmail("novoemailFatec@fatec.com"));
         //System.out.println(alunoDAO.getAlunoPorId(12L));
         //List<Integer> list = new LinkedList<>();
 
-       // AlunoDTO alunoDTO = new AlunoDTO(13L, "nome", "", "", 2L,list);
+        //AlunoDTO alunoDTO = new AlunoDTO(13L, "nome", "", "", 2L,list);
         //TurmaDTO turmaDTO = new TurmaDTO(7L, 2023,1,2);
 
-       // alunoDAO.addMatriculaAluno(alunoDTO, turmaDTO);
+        //alunoDAO.addMatriculaAluno(alunoDTO, turmaDTO);
 
-       // List<AlunoDTO> listaAlunos = alunoDAO.getAllAlunosPorTurma(TurmaDAO.getTurmaPorAtributo(new TurmaDTO(2023,1,1)));
+        //List<AlunoDTO> listaAlunos = alunoDAO.getAllAlunosPorTurma(TurmaDAO.getTurmaPorAtributo(new TurmaDTO(2023,1,1)));
 
        // System.out.println(listaAlunos);
     }
