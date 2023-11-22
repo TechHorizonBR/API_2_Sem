1021760558d44e5b06b1703ed7ca664e2d6bcbee
BeatrizPlacido <127264395+BeatrizPlacido@users.noreply.github.com>
2023-11-22 00:56:23 -0300

    :heavy_plus_sign: 4.04.01 build: Criação  do Controller  da tela de acompanhamento

--- TGSync/src/main/java/com/tgsync/tgsync/TelaResultadosDeEntregasController.java
+++ TGSync/src/main/java/com/tgsync/tgsync/TelaResultadosDeEntregasController.java
@@ -1,4 +1,24 @@
 package com.tgsync.tgsync;
 
-public class TelaResultadosDeEntregasController {
+import Model.DTO.AlunoDTO;
+import com.tgsync.tgsync.util.MudancaTelas;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.image.ImageView;
+
+public class TelaResultadosDeEntregasController extends MudancaTelas {
+
+    @FXML
+    private TableColumn<AlunoDTO, String> colunaEntrega;
+    private TableColumn<AlunoDTO, String> colunaFeedback;
+
+    @FXML
+    private ImageView imgLogo;
+
+    ObservableList<AlunoDTO> listEntrega = FXCollections.observableArrayList();
+
+
+
 }
