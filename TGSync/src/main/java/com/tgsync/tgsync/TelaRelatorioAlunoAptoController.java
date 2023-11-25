b9fd3b8947c4c9f665437c0a000142191bcf7000
Gilvane Amaro <gilvane.amaro.jr@gmail.com>
2023-11-24 22:12:12 -0300

    finalizando alunosaptos

--- TGSync/src/main/java/com/tgsync/tgsync/TelaRelatorioAlunoAptoController.java
+++ TGSync/src/main/java/com/tgsync/tgsync/TelaRelatorioAlunoAptoController.java
@@ -1,20 +1,17 @@
 package com.tgsync.tgsync;
 
-import Model.DAO.AlunoDAO;
-import Model.DAO.EntregaDAO;
-import Model.DAO.NotaDAO;
-import Model.DAO.TurmaDAO;
+import Model.DAO.*;
 import Model.DTO.*;
 import Model.util.Alerts;
 import com.tgsync.tgsync.util.MudancaTelas;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.io.IOException;
 import java.net.URL;
@@ -34,16 +31,23 @@ public class TelaRelatorioAlunoAptoController extends MudancaTelas implements In
     public Button gerarCsvButton;
     private ObservableList<Integer> obsAno = FXCollections.observableArrayList();
     private ObservableList<Integer> obsSemestre = FXCollections.observableArrayList();
-    List<Long> listMatricula = new LinkedList<>();
+    private List<Long> listMatriculaTurma1 = new LinkedList<>();
+    private List<Long> listMatriculaTurma2 = new LinkedList<>();
 
-    private EntregaDAO entregaDAO = new EntregaDAO();
-    public Double calculaMedia(List<Double> listaNotas){
-        Double soma =0.0;
-        for(Double nota: listaNotas){
-            soma += nota;
-        }
-        return soma/listaNotas.size();
-    }
+    private List<Long> listAlunosAptos = new LinkedList<>();
+    private ObservableList<AlunoDTO> obsAlunosAptos = FXCollections.observableArrayList();
+    @FXML
+    private TableColumn<AlunoDTO,String> colunaNome;
+
+    @FXML
+    private TableColumn<AlunoDTO,String> colunaTipoTg;
+
+    @FXML
+    private TableColumn<AlunoDTO,String> colunaOrientador;
+
+    @FXML
+    private TableView<AlunoDTO> tableAlunos;
+    private TGDAO tgdao = new TGDAO();
 
     public void encontraAlunoApto(ActionEvent event) {
         AlunoDAO alunoDAO = new AlunoDAO();
@@ -55,54 +59,123 @@ public class TelaRelatorioAlunoAptoController extends MudancaTelas implements In
             if (anoComboBox.getValue() != null && semestreComboBox.getValue() != null) {
                 TurmaDTO turmaDTO1 = TurmaDAO.getTurmaPorAtributo(new TurmaDTO(anoComboBox.getValue(), semestreComboBox.getValue(), 1));
                 TurmaDTO turmaDTO2 = TurmaDAO.getTurmaPorAtributo(new TurmaDTO(anoComboBox.getValue(), semestreComboBox.getValue(), 2));
-                List<EntregaDTO> listEntregasPort = new ArrayList<>();
-                List<EntregaDTO> listEntregasRelatorio = new ArrayList<>();
-                TGDTO tgdto = new TGDTO();
-                tgdto.setTipo("Portfólio");
-                if (turmaDTO1 != null && turmaDTO2 != null) {
-                    listEntregasPort.addAll(entregaDAO.getEntregasPorIdTurmaTipoTG(turmaDTO1, tgdto));
-                    listEntregasPort.addAll(entregaDAO.getEntregasPorIdTurmaTipoTG(turmaDTO2, tgdto));
-                    System.out.println(listEntregasPort.size());
-                }
-                tgdto.setTipo("Relatório Técnico");
-                if (turmaDTO1 != null && turmaDTO2 != null) {
-                    listEntregasRelatorio.addAll(entregaDAO.getEntregasPorIdTurmaTipoTG(turmaDTO1, tgdto));
-                    listEntregasRelatorio.addAll(entregaDAO.getEntregasPorIdTurmaTipoTG(turmaDTO2, tgdto));
-                }
-                List<String> tipoTg = new ArrayList<>();
-                tipoTg.add("Portfólio");
-                tipoTg.add("Relatório Técnico");
-                List<NotaDTO> listNota = new LinkedList<>();
 
-                for (String tipo : tipoTg) {
-                    listMatricula = alunoDAO.getAllMatriculaPorIdTipoeIdTurma(tipo, turmaDTO1);
-                    listIdEntregas = entregaDAO.getIdEntregasPorTurma(turmaDTO1.getId(), tipo);
 
-                    if (!listMatricula.isEmpty()) {
-                        for (Long matricula : listMatricula) {
-                            List<Double> notas = new LinkedList<>();
-                            notas = notaDAO.getMedia(listIdEntregas, matricula);
-                            Double media = 0.0;
 
-                            if (!notas.isEmpty()) {
-                                for (Double nota : notas) {
-                                    media += nota;
+                String tipo = "Relatório";
+
+                listMatriculaTurma2 = alunoDAO.getAllMatriculaPorIdTipoeIdTurma(tipo, turmaDTO1);
+                listIdEntregas = entregaDAO.getIdEntregasPorTurma(turmaDTO1.getId(), tipo);
+
+                boolean validacao = false;
+                if (!listMatriculaTurma2.isEmpty()) {
+                    for (Long matricula : listMatriculaTurma2) {
+                        for (Long idEntrega : listIdEntregas) {
+                            NotaDTO notaDTO = notaDAO.getNotaPorAlunoEntrega(matricula, idEntrega);
+                            if (notaDTO != null) {
+                                if (!listAlunosAptos.contains(matricula)) {
+                                    listAlunosAptos.add(matricula);
                                 }
-                                media /= listIdEntregas.size();
-                                DecimalFormat df = new DecimalFormat("#.##");
-                                String numeroFormatado = df.format(media);
-                                numeroFormatado = numeroFormatado.replace(",", ".");
-                                media = Double.valueOf(numeroFormatado);
-                                System.out.println(media);
+                                validacao = true;
                             }
-                            listNota.add(new NotaDTO(media, matricula));
+                            else{
+                                validacao = false;
+                            }
+                            if (!validacao) {
+                                try {
+                                    listAlunosAptos.remove(matricula);
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+                                }
+                                break;
+                            }
                         }
                     }
                 }
-                Alerts.showAlert("Sucesso!", "", "Gerado com sucesso!", Alert.AlertType.CONFIRMATION);
+
+                for(Long idAluno:listAlunosAptos){
+                    List<Double> notas = notaDAO.getMedia(listIdEntregas,idAluno);
+                    Double media = 0.0;
+                    if (!notas.isEmpty()) {
+                        for (Double nota : notas) {
+                            media += nota;
+                        }
+
+                        media /= listIdEntregas.size();
+                        if(media >= 6.0 ){
+                            obsAlunosAptos.add(alunoDAO.getAlunoPorId(idAluno));
+                        }
+                    }
+                }
+
+                tipo = "Portfólio";
+
+                listMatriculaTurma2 = alunoDAO.getAllMatriculaPorIdTipoeIdTurma(tipo, turmaDTO2);
+                listIdEntregas = entregaDAO.getIdEntregasPorTurma(turmaDTO2.getId(), tipo);
+
+                validacao = false;
+                if (!listMatriculaTurma2.isEmpty()) {
+                    for (Long matricula : listMatriculaTurma2) {
+                        for (Long idEntrega : listIdEntregas) {
+                            NotaDTO notaDTO = notaDAO.getNotaPorAlunoEntrega(matricula, idEntrega);
+                            if (notaDTO != null) {
+                                if (!listAlunosAptos.contains(matricula)) {
+                                    listAlunosAptos.add(matricula);
+                                }
+                                validacao = true;
+                            }
+                            else{
+                                validacao = false;
+                            }
+                            if (!validacao) {
+                                try {
+                                    listAlunosAptos.remove(matricula);
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+                                }
+                                break;
+                            }
+                        }
+                    }
+                }
+
+                for(Long idAluno:listAlunosAptos){
+                    List<Double> notas = notaDAO.getMedia(listIdEntregas,idAluno);
+                    Double media = 0.0;
+                    if (!notas.isEmpty()) {
+                        for (Double nota : notas) {
+                            media += nota;
+                        }
+
+                        media /= listIdEntregas.size();
+                        if(media >= 6.0 ){
+                            obsAlunosAptos.add(alunoDAO.getAlunoPorId(idAluno));
+                        }
+                    }
+                }
             }
+
+            colunaNome.setCellValueFactory(new PropertyValueFactory<>("nome"));
+            colunaOrientador.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getNomeOrientador()));
+            colunaTipoTg.setCellValueFactory(cellData -> {
+                AlunoDTO aluno = cellData.getValue();
+                List<TGDTO> tgDoAluno = tgdao.getTgsPorIdAluno(aluno.getId());
+                if (!tgDoAluno.isEmpty()) {
+                    for(TGDTO tgdto : tgDoAluno){
+                        return new SimpleStringProperty(tgdto.getTipo());
+                    }
+                } else {
+                    return new SimpleStringProperty("");
+                }
+                return null;
+            });
+            tableAlunos.setItems(obsAlunosAptos);
+
+
+            Alerts.showAlert("Sucesso!", "", "Gerado com sucesso!", Alert.AlertType.CONFIRMATION);
+
         } catch (Exception e){
-            Alerts.showAlert("Erro","","Erro ao gerar csv", Alert.AlertType.ERROR);
+            Alerts.showAlert("Erro",e.getMessage(),"Erro ao gerar csv", Alert.AlertType.ERROR);
         }
     }
     public void exibirSemestre(){
