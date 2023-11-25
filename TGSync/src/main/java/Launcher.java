7e80fdde8cfb15785574767623984846d7b4c655
santosjhony12 <santosdesouzajhony@gmail.com>
2023-11-25 16:57:05 -0300

    :feat: Adicionando executador e manual de uso

--- TGSync/src/main/java/Launcher.java
+++ TGSync/src/main/java/Launcher.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class Launcher {
+import com.tgsync.tgsync.HelloApplication;
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
 }
